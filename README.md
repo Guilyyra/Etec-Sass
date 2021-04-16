ade9729932ccf74f03bfde5f8c5068f98c7f165c
Guilherme José <guilherme.2010.guilherme@gmail.com>
2021-04-15 21:05:16 -0300

    Convertendo código CSS para SASS

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Etec-Sass
+# Etec Horácio Augusto da Silveira
+## Protótipo do site da Etec Horácio Augusto da Silveira
+
+O site da Etec Horácio nos próximos anos será o mais importante meio de comunicação das comunidades: Estudantil, Docente e entorno de suas Unidades Educacionais.
+
+![](img/about.png)
+
+## Desenvolvedores
+
+#### Ralf Dewrich Ferreira:
+
+* **Twitter:** [@ralfzeira](https://twitter.com/ralfzeira)
+* **Discord:** ralf#6175
+* **Email:** ralfferreira995@gmail.com
+
+#### Guilherme José de Lira Brito:
+
+* **Twitter:** [@guilyyra](https://twitter.com/guilyyra) 
+* **Discord:** Guilyyra#9404
+* **Email:** guilherme.2010.guilherme@gmail.com
---
+++ js/institucional.js
@@ -0,0 +1,161 @@
+function apmAbrir() {
+    var x = document.getElementById("apmAbre");
+    var y = document.getElementById("cipaAbre");
+    var z = document.getElementById("conselhoAbre");
+    var a = document.getElementById("depAbre");
+    var b = document.getElementById("gremAbre");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "none";
+    }
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "none";
+    }
+    if (a.style.display === "block") {
+      a.style.display = "none";
+    } else {
+      a.style.display = "none";
+    }
+    if (b.style.display === "block") {
+      b.style.display = "none";
+    } else {
+      b.style.display = "none";
+    }
+  }
+
+function cipaAbrir() {
+  var x = document.getElementById("apmAbre");
+    var y = document.getElementById("cipaAbre");
+    var z = document.getElementById("conselhoAbre");
+    var a = document.getElementById("depAbre");
+    var b = document.getElementById("gremAbre");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "none";
+    }
+    if (a.style.display === "block") {
+      a.style.display = "none";
+    } else {
+      a.style.display = "none";
+    }
+    if (b.style.display === "block") {
+      b.style.display = "none";
+    } else {
+      b.style.display = "none";
+    }
+}
+function conselhoAbrir() {
+  var x = document.getElementById("apmAbre");
+    var y = document.getElementById("cipaAbre");
+    var z = document.getElementById("conselhoAbre");
+    var a = document.getElementById("depAbre");
+    var b = document.getElementById("gremAbre");
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "block";
+    }
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "none";
+    }
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+    if (a.style.display === "block") {
+      a.style.display = "none";
+    } else {
+      a.style.display = "none";
+    }
+    if (b.style.display === "block") {
+      b.style.display = "none";
+    } else {
+      b.style.display = "none";
+    }
+}
+function depAbrir() {
+  var x = document.getElementById("apmAbre");
+    var y = document.getElementById("cipaAbre");
+    var z = document.getElementById("conselhoAbre");
+    var a = document.getElementById("depAbre");
+    var b = document.getElementById("gremAbre");
+    if (a.style.display === "block") {
+      a.style.display = "none";
+    } else {
+      a.style.display = "block";
+    }
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "none";
+    }
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "none";
+    }
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+    if (b.style.display === "block") {
+      b.style.display = "none";
+    } else {
+      b.style.display = "none";
+    }
+}
+function gremAbrir() {
+  var x = document.getElementById("apmAbre");
+    var y = document.getElementById("cipaAbre");
+    var z = document.getElementById("conselhoAbre");
+    var a = document.getElementById("depAbre");
+    var b = document.getElementById("gremAbre");
+    if (b.style.display === "block") {
+      b.style.display = "none";
+    } else {
+      b.style.display = "block";
+    }
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "none";
+    }
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      z.style.display = "none";
+    }
+    if (a.style.display === "block") {
+      a.style.display = "none";
+    } else {
+      a.style.display = "none";
+    }
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "none";
+    }
+}
